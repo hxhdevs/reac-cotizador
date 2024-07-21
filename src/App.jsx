bb9707aff9efc981927b9481d5ba72d8526d7d19
HXHdevs <jordisonnicolas@gmail.com>
2024-07-21 10:03:40 -0600

    Añadiendo botones de incremento y decrementos, agregando las funcionalidades a los botones

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,44 @@ function App() {
       setCantidad(+e.target.value);
   }
 
+  function handleClickDecremento(){
+    const valor = cantidad-STEP;
+    if(valor < MIN){
+      alert('Cantidad no valida');
+      return;
+    } 
+    setCantidad(valor)
+  }
+
+  function handleClickIncremento(){
+    const valor = cantidad+STEP;
+    if(valor>MAX){
+      alert('Cantidad no valida');
+      return;
+    } 
+    setCantidad(valor)
+  }
   return (
     // Este espacio es solo para presentacion
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-      <Header/>    
+      <Header />
 
+      <div className="flex justify-between my-6">
+        <button
+          type='button'
+          className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ringoffset-2 hover:ring-lime-500'
+          onClick={handleClickDecremento}
+        >-</button>
+        
+        <button
+          type='button'
+          className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ringoffset-2 hover:ring-lime-500'
+          onClick={handleClickIncremento}
+        >+</button>
+      </div>
+      
       <input
-        type="range"
+        type='range'
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
         onChange={handleChange}//esta linea viene siendo el equivalente a las dos lineas que siguen del cierre de function App
         min={MIN}
