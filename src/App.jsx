e12de23353f3ef103be73561a5779fa565679d36
HXHdevs <jordisonnicolas@gmail.com>
2024-07-21 00:35:55 -0600

    Añadiendo un input para las cantidades

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ function App() {
     // Este espacio es solo para presentacion
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header/>    
+
+      <input
+        type="range"
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      />
     </div>
   )
 }
