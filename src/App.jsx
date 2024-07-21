2290e881d826e309de686599ed911592e9de2e9b
HXHdevs <jordisonnicolas@gmail.com>
2024-07-21 00:16:59 -0600

    Aprendiendo como funciona JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 function App() {
+  // Aqui podemos colocar JS y pasarlo al return
+  const hola='Hola mundo';
+  const auth = false;
   return (
-    <h1 className="text-6xl font-extrabold">Hola Mundo</h1>
+    // Este espacio es solo para presentacion
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      {hola}
+      
+      {auth? 'autenticado':'no autenticado'}
+    </div>
   )
 }
 
